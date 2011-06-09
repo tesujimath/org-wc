e75136944dabc35ff4d7eca0cb4a1cc829f701e7
Eric Abrahamsen <eric@ericabrahamsen.net>
2011-06-09 01:33:59 +0000

    org-wc.el v4 (fork)
    Simon Guest writes:
    > At Fri, 29 Apr 2011 10:56:51 -0400, Nick Dokos wrote:
    >> Indeed: it would require a bit of refactoring of Simon's code to provide the
    >> function(s) to apply to each entry, and changes to the top level functions to
    >> use the mapping API instead of looping explicitly.
    >
    > That sounds like a good idea.  I may at some stage want to exclude
    > counting certain trees, and then I may have another look at this if
    > someone else hasn't already done it.
    >
    > But for now, time pressure dictates I stop hacking on my word count
    > function.
    >
    > cheers,
    > Simon
    
    I'm afraid this is a bit of a two-steps-forward, one-step-back
    situation, but I've rejiggered Simon's code so that it now:
    
    1. Uses the mapping and property APIs
    2. Allows selection of subtrees for count via a tag
    3. Sets wordcount totals for each subtree as a property, instead of an
       overlay
    
    First of all, this requires the fix to
    =org-end-of-meta-data-and-drawers= that I sent (and then re-sent) to
    this list earlier today. Otherwise it will "work funny".
    
    I changed it to use properties instead of overlays because I wanted
    something that was persistent, and available for programmatic
    manipulation. Plus, you can get an overlay effect with column view.
    
    So right now M-x org-word-count will do the following:
    
    1. Add the subtree word count as a property (=org-wc-prop-name=) to each
       headline in the buffer
    2. Respect the region, if it's active
    3. Operate only on trees tagged with =org-wc-include-tag=, if that tag
       is present
    4. Report a buffer/region word count total in the minibuffer
    5. With a prefix arg, *only* give a minibuffer report, don't set
       properties
    
    The two variables =org-wc-include-tag= and =org-wc-prop-name= are buffer
    local, unless I've misunderstood how buffer local works and screwed it
    up.
    
    There's a helper function, =org-wc-remove-all-props= that can be used to
    remove the =org-wc-prop-name= property from all headlines. Does anyone
    else think that =org-entry-delete= should remove the whole drawer if
    there are no other properties left?
    
    This is very much a proposal, and I've got a bit of time to work on it,
    so I'm willing to field requests, though my elisp is bad. Two immediate
    possibilities would be: automatically excluding subtrees tagged
    "noexport", and using =org-context= to be cleverer about what to avoid.
    
    Further suggestions (and code fixes) welcome!
    
    Eric
    
    http://permalink.gmane.org/gmane.emacs.orgmode/42663

--- org-wc.el
+++ org-wc.el
@@ -6,6 +6,8 @@
 ;;
 ;; Simon Guest, 23/4/11
 ;;
+;; Eric Abrahamsen, 8/6/11
+;;
 ;; Implementation based on:
 ;; - Paul Sexton's word count posted on org-mode mailing list 21/2/11.
 ;; - clock overlays
@@ -17,21 +19,46 @@
 ;; v3
 ;; 29/4/11
 ;; Handle narrowing correctly, so partial word count works on narrowed regions.
-(defun org-in-heading-line ()
-  "Is point in a line starting with `*'?"
-  (equal (char-after (point-at-bol)) ?*))
+;;
+;; v4
+;; 8/6/11
+;; Use mapping and property APIs, no more overlays
+
+(setq org-wc-include-tag "count"
+      org-wc-prop-name "WORDCOUNT")
+
+(make-variable-buffer-local 'org-wc-include-tag)
+(make-variable-buffer-local 'org-wc-prop-name)
 
 (defun org-word-count (beg end)
-  "Report the number of words in the Org mode buffer or selected region."
   (interactive "r")
-  (unless mark-active
-    (setf beg (point-min)
-          end (point-max)))
-  (let ((wc (org-word-count-aux beg end)))
-    (message (format "%d words in %s." wc
-                     (if mark-active "region" "buffer")))))
+  (let ((bmp (buffer-modified-p))
+	(tag (if (save-excursion
+		   (goto-char (point-min))
+		   (re-search-forward (concat "^\\*+[ \t].*" ":\\("
+					      org-wc-include-tag
+					      "\\):" "[^ \t\n]*[ \t]*$")
+				      nil t))
+		 org-wc-include-tag
+	       t)))
+    (save-restriction
+      (if (org-region-active-p)
+	  (narrow-to-region beg end))
+      (let* ((funky (if current-prefix-arg
+		      'org-wc-count-report
+		    'org-wc-count-add-props))
+	     (words (apply '+ (org-map-entries funky tag nil))))
+       (set-buffer-modified-p bmp)
+       (message (format "Counted %d words in %s." words
+			(if mark-active "region" "buffer")))))))
 
-(defun org-word-count-aux (beg end)
+(defun org-wc-count-add-props ()
+  (org-wc-aux t))
+
+(defun org-wc-count-report ()
+  (org-wc-aux nil))
+
+(defun org-wc-aux (props)
   "Report the number of words in the selected region.
 Ignores: heading lines,
          blocks,
@@ -42,128 +69,43 @@ LaTeX macros are counted as 1 word."
   (let ((wc 0)
         (block-begin-re "^#\\\+BEGIN")
         (block-end-re "^#\\+END")
-        (latex-macro-regexp "\\\\[A-Za-z]+\\(\\[[^]]*\\]\\|\\){\\([^}]*\\)}")
-        (drawers-re (concat "^[ \t]*:\\("
-                            (mapconcat 'regexp-quote org-drawers "\\|")
-                            "\\):[ \t]*$"))
-        (drawers-end-re "^[ \t]*:END:"))
-    (save-excursion
-      (goto-char beg)
-      (while (< (point) end)
-        (cond
-         ;; Ignore heading lines.
-         ((org-in-heading-line)
-          (forward-line))
-         ;; Ignore blocks.
-         ((looking-at block-begin-re)
-          (re-search-forward block-end-re))
-         ;; Ignore comments.
-         ((org-in-commented-line)
-          (forward-line))
-         ;; Ignore drawers.
-         ((looking-at drawers-re)
-          (re-search-forward drawers-end-re nil t))
-         ;; Count latex macros as 1 word, ignoring their arguments.
-         ((save-excursion
-            (backward-char)
-            (looking-at latex-macro-regexp))
-          (goto-char (match-end 0))
-          (setf wc (+ 2 wc)))
-         (t
-          (progn
-            (and (re-search-forward "\\w+\\W*" end 'skip)
-                 (incf wc)))))))
+	(latex-macro-regexp "\\\\[A-Za-z]+\\(\\[[^]]*\\]\\|\\){\\([^}]*\\)}"))
+    (let ((end (save-excursion
+		 (or (outline-next-heading)
+		     (point-max)))))
+      (save-excursion
+	(org-end-of-meta-data-and-drawers)
+	(while (< (point) end)
+	  (cond
+	   ((looking-at block-begin-re)
+	    (re-search-forward block-end-re))
+	   ;; Ignore comments.
+	   ((org-in-commented-line)
+	    (forward-line))
+	   ;; skip tables
+	   ((org-at-table-p)
+	    (forward-line))
+	   ;; Count latex macros as 1 word, ignoring their arguments.
+	   ((save-excursion
+	      (backward-char)
+	      (looking-at latex-macro-regexp))
+	    (goto-char (match-end 0))
+	    (setf wc (+ 2 wc)))
+	   (t
+	    (progn
+	      (and (re-search-forward "\\w+\\W*" end 'skip)
+		   (incf wc))))))))
+    (when props
+      (org-entry-put (point) org-wc-prop-name (number-to-string wc)))
     wc))
 
-(defun org-wc-count-subtrees ()
-  "Count words in each subtree, putting result as the property :org-wc on that
-heading."
+(defun org-wc-remove-prop ()
+  "Delete an existing wordcount property"
+  (org-entry-delete (point) org-wc-prop-name))
+
+(defun org-wc-remove-all-props ()
+  "Delete wordcount property from all headlines in buffer"
   (interactive)
-  (remove-text-properties (point-min) (point-max)
-                          '(:org-wc t))
-  (save-excursion
-    (goto-char (point-max))
-    (while (outline-previous-heading)
-      (save-restriction
-        (org-narrow-to-subtree)
-        (let ((wc (org-word-count-aux (point-min) (point-max))))
-          (put-text-property (point) (point-at-eol) :org-wc wc)
-          (goto-char (point-min)))))))
-
-(defun org-wc-display (beg end total-only)
-  "Show subtree word counts in the entire buffer.
-With prefix argument, only show the total wordcount for the buffer or region
-in the echo area.
-
-Use \\[org-wc-remove-overlays] to remove the subtree times.
-
-Ignores: heading lines,
-         blocks,
-         comments,
-         drawers.
-LaTeX macros are counted as 1 word."
-  (interactive "r\nP")
-  (org-wc-remove-overlays)
-  (unless total-only
-    (let ((bmp (buffer-modified-p))
-          wc
-          p)
-      (org-wc-count-subtrees)
-      (save-excursion
-        (goto-char (point-min))
-        (while (or (and (equal (setq p (point)) (point-min))
-                        (get-text-property p :org-wc))
-                   (setq p (next-single-property-change
-                            (point) :org-wc)))
-          (goto-char p)
-          (when (setq wc (get-text-property p :org-wc))
-            (org-wc-put-overlay wc (funcall outline-level))))
-        ;; Arrange to remove the overlays upon next change.
-        (when org-remove-highlights-with-change
-          (org-add-hook 'before-change-functions 'org-wc-remove-overlays
-                        nil 'local)))
-    (set-buffer-modified-p bmp)))
-  (if mark-active
-      (org-word-count beg end)
-    (org-word-count (point-min) (point-max))))
-
-(defvar org-wc-overlays nil)
-(make-variable-buffer-local 'org-wc-overlays)
-
-(defun org-wc-put-overlay (wc &optional level)
-  "Put an overlays on the current line, displaying word count.
-If LEVEL is given, prefix word count with a corresponding number of stars.
-This creates a new overlay and stores it in `org-wc-overlays', so that it
-will be easy to remove."
-  (let* ((c 60)
-         (l (if level (org-get-valid-level level 0) 0))
-         (off 0)
-         ov tx)
-    (org-move-to-column c)
-    (unless (eolp) (skip-chars-backward "^ \t"))
-    (skip-chars-backward " \t")
-    (setq ov (make-overlay (1- (point)) (point-at-eol))
-          tx (concat (buffer-substring (1- (point)) (point))
-                     (make-string (+ off (max 0 (- c (current-column)))) ?.)
-                     (org-add-props (format "%s" (number-to-string wc))
-                         (list 'face 'org-wc-overlay))
-                     ""))
-    (if (not (featurep 'xemacs))
-        (overlay-put ov 'display tx)
-      (overlay-put ov 'invisible t)
-      (overlay-put ov 'end-glyph (make-glyph tx)))
-    (push ov org-wc-overlays)))
-
-(defun org-wc-remove-overlays (&optional beg end noremove)
-  "Remove the occur highlights from the buffer.
-BEG and END are ignored.  If NOREMOVE is nil, remove this function
-from the `before-change-functions' in the current buffer."
-  (interactive)
-  (unless org-inhibit-highlight-removal
-    (mapc 'delete-overlay org-wc-overlays)
-    (setq org-wc-overlays nil)
-    (unless noremove
-      (remove-hook 'before-change-functions
-                   'org-wc-remove-overlays 'local))))
+  (org-map-entries 'org-wc-remove-prop t 'file))
 
 (provide 'org-wc)
